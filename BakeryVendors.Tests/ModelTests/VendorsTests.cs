bb089911bf0e5500d4311ae35bfc75794d7f9587
Kim Brannian <brannian@gmail.com>
2021-12-17 13:44:17 -0800

    add third vendor test and pass

--- BakeryVendors.Tests/ModelTests/VendorsTests.cs
+++ BakeryVendors.Tests/ModelTests/VendorsTests.cs
@@ -28,7 +28,37 @@ namespace BakeryVendors.Tests
 
       Assert.AreEqual(name, resultName);
       Assert.AreEqual(vendorDescription, resultDesc);
-      
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      string name = "Test Vendor";
+      string vendorDescription = "Test Description";
+      Vendor newVendor = new Vendor(name, vendorDescription);
+    }
+
+
+      // [TestMethod]
+      // public void GetAll_ReturnsAllVendorObjects_VendorsList()
+      // {
+      //   string name = "Todd's Coffee";
+      //   string vendorDescription = "coffee shop";
+
+      //   string name2 = "Martha's Corner";
+      //   string vendorDescription2 = "specialty market";
+
+      //   Vendor newVendor = new Vendor(name, vendorDescription);
+      //   Vendor newVendor2 = new Vendor(name2, vendorDescription2);
+      //   List<Vendor> newList = new List<Vendor> { newVendor, newVendor2 };
+
+      //   List<Vendor> result = Vendor.GetAll();
+
+      //   CollectionAssert.AreEqual(newList, result);
+
+
+        
+      // }
+
     }
   }  
-}
