d711a7739e42246739774af14c319ddff8786d46
Kim Brannian <brannian@gmail.com>
2021-12-17 09:32:38 -0800

    debug first tests

---
+++ BakeryVendors.Tests/ModelTests/OrdersTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using BakeryVendors.Models;
+using System;
+
+namespace BakeryVendors.Models
+{
+  
+  [TestClass]
+    public class OrderTests
+    {
+
+      [TestMethod]
+      public void OrderConstructor_CreatesInstancOfOrder_Order()
+      {
+        Order addOrder = new Order("weekly", "baguettes", 50, "1/1/2021");
+        Assert.AreEqual(typeof(Order), addOrder.GetType());
+      }
+      }
+    
+  }
+  
+
+
+  
+
+
+
--- BakeryVendors/Models/Orders.cs
+++ BakeryVendors/Models/Orders.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic
+using System.Collections.Generic;
 
 namespace BakeryVendors.Models
 {
@@ -8,7 +8,15 @@ namespace BakeryVendors.Models
     public string OrderTitle { get; set; } 
     public string Description { get; set; }
     public int Price  { get; set; }
-
     public string Date { get; set; }
+
+  public Order(string orderTitle, string description, int price, string date)
+  {
+    OrderTitle = orderTitle;
+    Description = description;
+    Price = price;
+    Date = date;
   }
-}
+
+  }
+}  
