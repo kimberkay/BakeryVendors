646b24480f95858dabacbc25f875beb1a0c3fc1c
Kim Brannian <brannian@gmail.com>
2021-12-17 14:33:47 -0800

    add order controllers

--- BakeryVendors/Models/Order.cs
+++ BakeryVendors/Models/Order.cs
@@ -35,5 +35,10 @@ namespace BakeryVendors.Models
     _instances.Clear();
   }
 
+  public static Order Find(int searchId)
+  {
+    return _instances[searchId-1];
+  }
+
   }
 }  
--- BakeryVendors/Models/Vendor.cs
+++ BakeryVendors/Models/Vendor.cs
@@ -40,8 +40,5 @@ namespace BakeryVendors.Models
       Orders.Add(order);
     }
 
-    
-
-  
   }
 }
