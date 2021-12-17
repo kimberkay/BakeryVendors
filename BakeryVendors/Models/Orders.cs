75a401e996d9db3b1ef08d5d3544a2100c6a6ddd
Kim Brannian <brannian@gmail.com>
2021-12-17 11:53:16 -0800

    lunch break

--- BakeryVendors/Models/Orders.cs
+++ BakeryVendors/Models/Orders.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace BakeryVendors.Models
 {
@@ -10,12 +11,28 @@ namespace BakeryVendors.Models
     public int Price  { get; set; }
     public string Date { get; set; }
 
+    public int Id {get; }
+
+    private static List<Order> _instances = new List<Order> {};
+
   public Order(string orderTitle, string description, int price, string date)
   {
     OrderTitle = orderTitle;
     Description = description;
     Price = price;
     Date = date;
+    _instances.Add(this);
+    Id = _instances.Count;
+  }
+
+  public static List<Order> GetAll()
+  {
+    return _instances;
+  }
+
+  public static void ClearAll()
+  {
+    _instances.Clear();
   }
 
   }
