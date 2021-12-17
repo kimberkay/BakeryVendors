d1959fa6910272f923a1d91c8a9a9ea975fea12a
Kim Brannian <brannian@gmail.com>
2021-12-17 10:28:07 -0800

    add second test and pass

--- BakeryVendors.Tests/ModelTests/OrdersTests.cs
+++ BakeryVendors.Tests/ModelTests/OrdersTests.cs
@@ -16,7 +16,29 @@ namespace BakeryVendors.Tests
         Order addOrder = new Order("weekly", "baguettes", 50, "1/1/2021");
         Assert.AreEqual(typeof(Order), addOrder.GetType());
       }
+
+      //public Order(string orderTitle, string description, int price, string date)
+
+      [TestMethod]
+      public void GetOrder_ReturnsInstanceOfAnOrder_Order()
+      {
+        string orderTitle = "weekly";
+        string description = "baguettes";
+        int price = 50;
+        string date = "1/1/2021";
+
+        Order newOrder = new Order(orderTitle, description, price, date);
+        string resultTitle = newOrder.OrderTitle;
+        string resultDescription = newOrder.Description;
+        int resultPrice = newOrder.Price;
+        string resultDate = newOrder.Date;
+
+        Assert.AreEqual(orderTitle, resultTitle);
+        Assert.AreEqual(description, resultDescription);
+        Assert.AreEqual(price, resultPrice);
+        Assert.AreEqual(date, resultDate);
       }
+    }
     
   }
   
